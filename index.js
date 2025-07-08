fbf2ca0857a6234c712d556177005bd9e3392cdc
Hyun <eric0909kim@gmail.com>
2025-07-08 09:31:08 -0400

    configured db connection

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 require('dotenv').config();
+const connectDB = require('./config/db');
 
 const app = express();
+connectDB();
+
 app.listen(process.env.PORT, () => {
     console.log(`Connected to port ${process.env.PORT}`);
 })
