2fd69f04befa8db6ccb73a69986c510993cb3357
Hyun <eric0909kim@gmail.com>
2025-07-10 10:45:31 -0400

    Update index.js to include capture raw body and verify webhook

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ require('dotenv').config();
 
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
+const captureRawBody = require('./middleware/rawBody');
+const verifyShopifyWebhook = require('./middleware/verifyWebhook');
 
 const app = express();
 
 connectDB();
 
 app.use('/auth', authRoutes);
+app.use(captureRawBody);
+app.use(express.json());
+app.use(verifyShopifyWebhook);
 
 app.listen(process.env.PORT, () => {
     console.log(`Connected to port ${process.env.PORT}`);
