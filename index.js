01373bd2bccece17ce1faea3039ef97494f7f0ab
Hyun <eric0909kim@gmail.com>
2025-09-02 09:11:52 -0400

    clean up index.js

--- index.js
+++ index.js
@@ -3,19 +3,15 @@ require('dotenv').config();
 
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
-const captureRawBody = require('./middleware/rawBody');
-const verifyShopifyWebhook = require('./middleware/verifyWebhook');
 const shopifyWebhook = require('./routes/webhook');
 
 const app = express();
 
 connectDB();
+app.use(express.json());
 
 app.use('/auth', authRoutes);
-app.use(captureRawBody);
-app.use(express.json());
-app.use(verifyShopifyWebhook);
-app.use(shopifyWebhook);
+app.use('webhook', shopifyWebhook);
 
 app.listen(process.env.PORT, () => {
     console.log(`Connected to port ${process.env.PORT}`);
